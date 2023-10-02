47c1878c25dc8b1f4cbc025bda6a5e7f6e491bf8
Leon Wieser <leon.tobias.steiermark@gmail.com>
2023-10-02 15:34:12 +0200

    minor changes

--- main.cpp
+++ main.cpp
@@ -8,10 +8,8 @@ int main()
     int sli = 5;
     // cout << "How many slices?: ";
     // cin >> sli;
-
     Game *g = new Game();
     g->setup(sli);
-    // g->move(TowerA, TowerB);
     g->solve();
     delete g;
     return 0;
